b9fd09d6c0fd6a272b0639a91ee4716d248a7aa2
Ralph Desiderio <ralphd60@gmail.com>
2024-03-25 20:06:42 -0400

    Update official copy

--- GetData.py
+++ GetData.py
@@ -1,28 +1,47 @@
 import requests
 import browser_cookie3
-
+import re
 from bs4 import BeautifulSoup
 
 
-def get_detail():
-    print('hello')
+def get_detail(print_key):
+
     # https://gis.fultoncountyny.gov/imo/propdetail.aspx?swis=173089&printkey=08801600010360000000
     # create response object
     # r = requests.get(url + filename)
-    #url = 'https://gis.fultoncountyny.gov'
-
+    url = 'https://gis.fultoncountyny.gov'
+    # only need this to grab the cookies.  not able to pass it properly
+    # but can keep trying
     #https://github.com/borisbabic/browser_cookie3
-    cj = browser_cookie3.chrome(domain_name='gis.fultoncountyny.gov')
+    #cj = browser_cookie3.chrome(domain_name= url)
 
-    r = requests.get('https://gis.fultoncountyny.gov/imo/propdetail.aspx?printkey=08801600010060000000&swis=173089',cookies={"HasAgreedToDisclaimer":"True","ASP.NET_SessionId":"tkyli455ltwnc245gxkkli55"})
-
-
-    print(cj)
-    print(r.text)
+    r = requests.get(url + '/imo/propdetail.aspx?printkey=' +  print_key + '&swis=173089',
+                     cookies={"HasAgreedToDisclaimer":"True","ASP.NET_SessionId":"cnmv2g55mk04g4a5qzfacj55"})
 
     # create beautiful-soup object
-    # soup = BeautifulSoup(r.content, "xml")
-    #print(soup.prettify())
+    soup = BeautifulSoup(r.content, "lxml")
 
-if __name__ == '__main__':
-    get_detail()
+    find_div = soup.find("div", class_="centerColumn", id="pnlRTaxID")
+
+    find_table = find_div.find("table", class_="reportTable singleBlack")
+    table_rows = find_table.find_all('tr')
+
+    # #THis pulls out just one piece of text data
+    for i in table_rows:
+        zone_data = i.find_all('span', id='lblZoningCode')
+        for j in zone_data:
+            return j.text
+    # # This grabs the label text and the result text
+    # for i in table_rows:
+    #     table_data = i.find_all("td", class_="headerTD")
+    #     table_data = table_data + i.find_all('span')
+    #     #data becomes a irregular list
+    #     data = [re.sub('\W+',' ', j.text) for j in table_data]
+    #     for x in range(len(data)):
+    #         print(data[x])
+
+
+
+
+# if __name__ == '__main__':
+#     get_detail()
--- main.py
+++ main.py
@@ -1,28 +1,31 @@
-# This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from GetData import get_detail
 import pandas as pd
+import logging
 
+logging.basicConfig(filename='c:\\temp\\assessment.log', filemode='w', format='%(asctime)s %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-def ImportData(assessdata):
+def import_data(assess_data):
 
-    df_assessed = pd.read_csv(assessdata)
-    df_assessed['printkey'] = ''
+    df_assessed = pd.read_csv(assess_data)
+    df_assessed['key_code'] = ''
+    df_assessed['zoning_code'] = ''
     for i, row in df_assessed.iterrows():
         search_key = ''
         temp_key = ''  # temp_key holds the data until we perform padding
-        x = 0 # used to perfom the first 2 sections of the TaxKey
-        TaxKey = row['Print Key'] # TaxKey is the Tax Map code on the tax roll
+        x = 0  # used to perfom the first 2 sections of the TaxKey
+        tax_key = row['Print Key']  # TaxKey is the Tax Map code on the tax roll
 
         # print(TaxKey)
-
         count = 0
-        for key in TaxKey:
-            lengthTaxkey = len(TaxKey)
+        for key in tax_key:
+            length_tax_key = len(tax_key)
             count += 1
 
-            if x !=2:
+            if x != 2:
                 if key.isdigit():
                     temp_key += key
                 else:
@@ -31,9 +34,7 @@ def ImportData(assessdata):
                     temp_key = ''
                     # print(real_key)
                     x += 1
-
-
-            else: # this pads the 3rd item with 4
+            else:  # this pads the 3rd item with 4
                 if key.isdigit():
                     temp_key += key
                 else:
@@ -42,19 +43,24 @@ def ImportData(assessdata):
                     temp_key = ''
                     # print(real_key)
                     x += 1
-            if count == lengthTaxkey:
+            if count == length_tax_key:
                 real_key = temp_key.zfill(3)
                 search_key += real_key + '0000'
                 # print(real_key)
         if len(search_key) != 20:
             search_key += '000'
-
+        print(tax_key)
         print(search_key)
-        df_assessed['printkey'][i]= search_key
 
-    print(df_assessed[['Print Key', 'printkey']].head())
-    print(df_assessed[['Print Key', 'printkey']].tail())
-# Press the green button in the gutter to run the script.
+        try:
+            zone_code = get_detail(search_key)
+            df_assessed['key_code'][i] = search_key
+            df_assessed['zoning_code'][i] = zone_code
+        except:
+            logging.info('Map key ' + tax_key)
+
+    df_assessed.to_csv("c:\\temp\\FultonAssesScrubbed.csv")
+
+
 if __name__ == '__main__':
-    ImportData("c:\\temp\FULTON_AssessmentRole.csv")
-
+    import_data("c:\\temp\\FULTON_AssessmentRole.csv")
